8f8396014c1db4392cb8a061b3c10539b4596fd2
Arnaud Mazelin <mazelin.arnaud@gmail.com>
2018-01-14 10:23:01 +0100

    add UT UserSpecification

---
+++ rental-mgr-core/src/test/java/com/nilezam/rentalmgr/model/user/UserSpecificationTest.java
@@ -0,0 +1,29 @@
+package com.nilezam.rentalmgr.model.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserSpecificationTest {
+
+
+    @Test
+    public void whenMailEqualSpecification_thenReturnTrue() {
+
+        //Given
+        final String mail = "john.doe@anonymous.com";
+        User user = new User(1L, "John", "Doe", mail, "123");
+        final UserSpecification.MailEqual mailEqual = new UserSpecification.MailEqual(mail);
+
+        //When
+        final boolean specified = mailEqual.isSpecifiedBy(user);
+
+        //Then
+        assertTrue(specified);
+    }
+
+
+}
