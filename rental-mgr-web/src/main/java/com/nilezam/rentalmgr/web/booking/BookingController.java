196ff1430d785cba07327a1417b87062994de176
Arnaud mazelin <mazelin.arnaud@gmail.com>
2018-05-21 17:33:47 +0200

    add save Bookings

---
+++ rental-mgr-web/src/main/java/com/nilezam/rentalmgr/web/booking/BookingController.java
@@ -0,0 +1,49 @@
+package com.nilezam.rentalmgr.web.booking;
+
+
+import com.nilezam.rentalmgr.model.booking.Booking;
+import com.nilezam.rentalmgr.persistence.booking.BookingRepository;
+import com.nilezam.rentalmgr.persistence.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BookingController {
+
+    private final BookingRepository bookingRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public BookingController(BookingRepository bookingRepository, UserRepository userRepository) {
+        this.bookingRepository = bookingRepository;
+        this.userRepository = userRepository;
+    }
+
+
+    @RequestMapping(value="/bookings/{bookingId}")
+    public ResponseEntity<Booking> getBookings(@PathVariable Long bookingId) {
+
+        return bookingRepository.get(bookingId)
+                .map(booking -> new ResponseEntity<>(booking, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
+    }
+
+    @RequestMapping(value = "/bookings", method = RequestMethod.POST)
+    public ResponseEntity<Booking> addBooking(@RequestBody BookingForm bookingForm) {
+        userRepository.get(bookingForm.getUserId()).map(user -> saveBooking(bookingForm));
+
+        return null;
+
+    }
+
+    private Booking saveBooking(@RequestBody BookingForm bookingForm) {
+        return bookingRepository.add(new Booking.BookingBuilder().startDate(bookingForm.getStartDate()).build());
+    }
+}
---
+++ rental-mgr-web/src/main/java/com/nilezam/rentalmgr/web/booking/BookingForm.java
@@ -0,0 +1,53 @@
+package com.nilezam.rentalmgr.web.booking;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class BookingForm {
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private LocalDateTime createDate;
+    private Long userId;
+    private int peopleCount;
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public int getPeopleCount() {
+        return peopleCount;
+    }
+
+    public void setPeopleCount(int peopleCount) {
+        this.peopleCount = peopleCount;
+    }
+}
