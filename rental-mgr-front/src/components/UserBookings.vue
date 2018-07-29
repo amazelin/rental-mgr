2cba07cdc7bd4995ead80fa8e4db35ef04868f46
Arnaud mazelin <mazelin.arnaud@gmail.com>
2018-07-29 18:30:25 +0200

    add bookings table

--- rental-mgr-front/src/components/UserBookings.vue
+++ rental-mgr-front/src/components/UserBookings.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>
-    Hello
-  </div>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Created</th>
+        <th scope="col">Start date</th>
+        <th scope="col">End date</th>
+        <th scope="col">Status</th>
+      </tr>
+    </thead>
+    <tbody v-for="booking in bookings">
+      <tr>
+        <th scope="row">{{booking.id}}</th>
+        <td>{{booking.createDate}}</td>
+        <td>{{booking.startDate}}</td>
+        <td>{{booking.endDate}}</td>
+        <td>{{booking.status.label}}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
@@ -9,12 +26,12 @@
 export default {
   data() {
     return {
-      user: this.$store.getters.user
+      bookings: {}
     }
   },
   created: function(){
-    this.$http.get('http://localhost:8081/users/{id}', {params: {id: this.$store.getters.user.id}, headers: {'Authorization': this.$store.getters.user.token}}, ).then(response => {
-      this.user = response.body;
+    this.$http.get('http://localhost:8081/users/{id}/bookings', {params: {id: this.$store.getters.user.id}, headers: {'Authorization': this.$store.getters.user.token}}, ).then(response => {
+      this.bookings = response.body;
     }, response => {
      // error callback
     });
